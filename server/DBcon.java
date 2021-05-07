233cae0bc3eabed5002743c53cd5d7457d6e5127
jhon-rjf <ykjung0917@kakao.com>
2021-05-07 10:26:21 +0900

    DBcon 쓰레드에 넣기 성공, 하지만 쓰레드를 또 실행시키면 에러

--- server/DBcon.java
+++ server/DBcon.java
@@ -5,14 +5,18 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class DBcon {
+public class DBcon extends Thread {
 	
 	String Msg = null;
 	String driver = "org.mariadb.jdbc.Driver";
     Connection con;
     PreparedStatement pstmt;
     
-    public DBcon() {
+    public void run() {
+    	System.out.println("Thread start");
+    	
+    		
+    	
          try {
             Class.forName(driver); //드라이버 객체화
             con = DriverManager.getConnection( //db와 연결
@@ -22,13 +26,14 @@ public class DBcon {
             
             if( con != null ) {
                 System.out.println("DB 접속 성공");
-                //INSERT INTO chat VALUES("정윤걸", DEFAULT, "data");
-                pstmt = con.prepareStatement("INSERT INTO CHAT VALUES('admin',DEFAULT,'talk')");//준비완료
-             //   pstmt.setString(1, name);
-              //  pstmt.setString(1, buffer);
+                
+                pstmt = con.prepareStatement("INSERT INTO CHAT VALUES('admin',DEFAULT,?)");//준비완료
+                pstmt.setString(1, Msg);
+            
                 System.out.println(Msg);
                 pstmt.executeQuery();//쿼리문을 넣어라
-               // System.out.println(buffer);
+                Msg = null;
+             
             }
         
            
@@ -46,8 +51,9 @@ public class DBcon {
  			e.printStackTrace();
  			System.out.println("쿼리실패");
  		}
-         
-    }
+    	}      
+    
+    
     
    
    
